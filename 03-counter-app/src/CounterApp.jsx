1ab9dc74a9bd47cd771378c545d7832fc3b09d6a
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-01-17 11:04:55 -0500

    hook useState

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,21 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types'
 
 // [rafce} para hacer el esqueleo 
 
 const CounterApp = ( { value } ) => {
 
+    // useState retorna una arreglo con dos valores
+    const [ counter, setCounter ] = useState(0);
+
+
     //HandAdd
     const handleAdd = () => {
-        return () => console.log('hola mundo');
+        // return () => console.log('hola mundo');
+        // setCounter( counter + 1 );
+        setCounter( (c) => c +1)
     };
     
 
-    return (
+    return (    
         <Fragment>  
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button onClick={  handleAdd() } >+1</button>
+            <h2> { counter }</h2>
+            <button onClick={  handleAdd } >+1</button>
             {/* <button onClick={ (e) => { handleAdd(e) } }>+1</button> */}
         </Fragment>
       );
