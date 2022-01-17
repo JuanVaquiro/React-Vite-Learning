4e4514fad7df8b810621b70c751db73ee17fce7a
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-01-17 12:25:05 -0500

    intro reacr prt2

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -3,25 +3,35 @@ import PropTypes from 'prop-types'
 
 // [rafce} para hacer el esqueleo 
 
-const CounterApp = ( { value } ) => {
+const CounterApp = ( { value = 3 } ) => {
 
     // useState retorna una arreglo con dos valores
-    const [ counter, setCounter ] = useState(0);
+    const [ counter, setCounter ] = useState( value );
 
 
     //HandAdd
     const handleAdd = () => {
         // return () => console.log('hola mundo');
         // setCounter( counter + 1 );
-        setCounter( (c) => c +1)
+        setCounter( (c) => c + 1)
     };
     
+    const handleMeno = () => {
+        setCounter( (c) => c - 1)
+    };
 
+    const defValue = () => {
+         setCounter(value)                                                                                                                                                                                                                                                             
+    };
+    
+    
     return (    
         <Fragment>  
             <h1>CounterApp</h1>
-            <h2> { counter }</h2>
+            <h2> { counter }</h2>                           
             <button onClick={  handleAdd } >+1</button>
+            <button onClick={ defValue }>Reset</button>                                                                                
+            <button onClick={ handleMeno }>-1</button>
             {/* <button onClick={ (e) => { handleAdd(e) } }>+1</button> */}
         </Fragment>
       );
