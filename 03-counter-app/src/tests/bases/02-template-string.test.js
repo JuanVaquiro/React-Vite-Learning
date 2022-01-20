d8353cfc344f0851c3ceda3c229df430e5f777c0
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-01-20 08:59:50 -0500

    React testing library and eslint confing

--- 03-counter-app/src/tests/bases/02-template-string.test.js
+++ 03-counter-app/src/tests/bases/02-template-string.test.js
@@ -4,13 +4,13 @@ import { getSaludo } from "../../bases/02-template-string"
 
 describe('Pruebas en 02-teample-string', () => {
     
-    test('geSaludo retorna juan ', () => {
+    test('getSaludo retorna juan ', () => {
         
         const nombre = 'juan';
 
         const saludo = getSaludo( nombre );
 
-          expect( saludo ).toBe( 'hola' + nombre );
+          expect( saludo ).toBe( 'Hola ' + nombre );
     });
     
 
