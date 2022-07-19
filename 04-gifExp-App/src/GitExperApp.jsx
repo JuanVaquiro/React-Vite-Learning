68ba9f6fd2585234b732899f0e7dfcba837952f9
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-07-19 16:45:38 -0500

    cap-04 Git-Expert-App

---
+++ 04-gifExp-App/src/GitExperApp.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function GitExpertApp() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+       <h1>GitExpertApp</h1>
+    </div>
+  )
+}
+
+export default GitExpertApp
