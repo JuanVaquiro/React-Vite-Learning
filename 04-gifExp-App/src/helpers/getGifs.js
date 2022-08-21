3a2c243e4a5460a79a2528dd1333b7137356c166
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-08-21 10:41:30 -0500

    testing getGifs()

--- 04-gifExp-App/src/helpers/getGifs.js
+++ 04-gifExp-App/src/helpers/getGifs.js
@@ -1,18 +1,18 @@
-const getGifs = async ( category ) => {
+// import fetch from 'cross-fetch';
 
+const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=UWq2H92uOZoNgd5sj1D011B5dHLW1xoS&q=${category}&limit=9`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  // console.log(data)
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=UWq2H92uOZoNgd5sj1D011B5dHLW1xoS&q=${ category }&limit=9`
-    const resp = await fetch( url );
-    const { data } = await resp.json();
-    // console.log(data)
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  // console.log(gifs);
+  return gifs;
+};
 
-    const gifs = data.map( img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }))
-    console.log(gifs)
-    return gifs
-}   
-
-export default getGifs
+export default getGifs;
