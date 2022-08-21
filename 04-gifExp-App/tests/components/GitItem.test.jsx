fe7944abc007bb3d2877fb5ab9188bbac50e6198
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-08-21 09:31:03 -0500

    testing component GitItem

--- 04-gifExp-App/tests/components/GitItem.test.jsx
+++ 04-gifExp-App/tests/components/GitItem.test.jsx
@@ -1,9 +1,34 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import GitItem from "../../src/components/GitItem";
 
-describe('test en el compoenntes gititme', () => {
+describe('test en el compoenntes <GitItem />', () => {
+
+    const title = "Gifs";
+    const url = 'https://gifs.com/gif.png'
+
     test('debe de hacer match con el snapshot', () => {
         
-        render(<GitItem />)
+        const { container } = render(<GitItem  title={ title } url={ url } />)
+        
+        expect( container ).toMatchSnapshot()
+    
     });
-});
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        
+        render(<GitItem  title={ title } url={ url } />);
+        // screen.debug();  mostrar lo que imprime el compoenente
+        // expect( screen.getByRole('img').src ).toBe( url )
+
+        const { src, alt } = screen.getByRole('img')
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+        
+        render(<GitItem  title={ title } url={ url } />);
+        expect( screen.getByText( title ) ).toBeTruthy();
+    });
+
+ });
